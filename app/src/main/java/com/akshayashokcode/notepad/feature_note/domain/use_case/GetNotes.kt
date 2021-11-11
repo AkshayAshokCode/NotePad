0bbf9a79e8602d1977e5dc71b34410efd7e9c343
Riderpool1 <akshayashokan1054.com>
2021-11-11 18:05:59 +0530

    Issue#5 fix

--- app/src/main/java/com/akshayashokcode/notepad/feature_note/domain/use_case/GetNotes.kt
+++ app/src/main/java/com/akshayashokcode/notepad/feature_note/domain/use_case/GetNotes.kt
@@ -21,14 +21,14 @@ class GetNotes(
                     when(noteOrder){
                         is NoteOrder.Title-> notes.sortedBy { it.title.lowercase() }
                         is NoteOrder.Date-> notes.sortedBy { it.timeStamp }
-                      //  is NoteOrder.Color-> notes.sortedBy { it.color }
+                        is NoteOrder.Color-> notes.sortedBy { it.color }
                     }
                 }
                 is OrderType.Descending->{
                     when(noteOrder){
                         is NoteOrder.Title-> notes.sortedByDescending { it.title.lowercase() }
                         is NoteOrder.Date-> notes.sortedByDescending { it.timeStamp }
-                       // is NoteOrder.Color-> notes.sortedByDescending{ it.color }
+                        is NoteOrder.Color-> notes.sortedByDescending{ it.color }
                     }
                 }
             }
