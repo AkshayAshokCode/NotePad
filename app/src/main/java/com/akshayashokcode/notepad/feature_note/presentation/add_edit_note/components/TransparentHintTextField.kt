d5b9dddbeb19575c30857df64b69eaeb65a846c1
Riderpool1 <akshayashokan1054.com>
2021-11-12 15:29:25 +0530

    Issue#11 fix

--- app/src/main/java/com/akshayashokcode/notepad/feature_note/presentation/add_edit_note/components/TransparentHintTextField.kt
+++ app/src/main/java/com/akshayashokcode/notepad/feature_note/presentation/add_edit_note/components/TransparentHintTextField.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,6 +12,7 @@ import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardCapitalization
 
 
 @Composable
@@ -30,6 +32,7 @@ fun TransparentHintTextFiled(
             onValueChange = onValueChange,
             singleLine = singleLine,
             textStyle = textStyle,
+            keyboardOptions = KeyboardOptions.Default.copy(capitalization = KeyboardCapitalization.Sentences),
             modifier = Modifier
                 .fillMaxWidth()
                 .onFocusChanged { onFocusChange(it) }
@@ -57,6 +60,7 @@ fun TransparentHintTextFiledContent(
             onValueChange = onValueChange,
             singleLine = singleLine,
             textStyle = textStyle,
+            keyboardOptions = KeyboardOptions.Default.copy(capitalization = KeyboardCapitalization.Sentences),
             modifier = Modifier
                 .fillMaxSize()
                 .onFocusChanged { onFocusChange(it) }
