2582ee6c45de36cb00d713c2cc8a79e38ed8e95c
Riderpool1 <akshayashokan1054.com>
2021-11-24 11:07:05 +0530

    test cases

---
+++ app/src/androidTest/java/com/akshayashokcode/notepad/feature_note/presentation/notes/NotesScreenTest.kt
@@ -0,0 +1,70 @@
+package com.akshayashokcode.notepad.feature_note.presentation.notes
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.akshayashokcode.notepad.core.util.TestTags
+import com.akshayashokcode.notepad.di.AppModule
+import com.akshayashokcode.notepad.feature_note.presentation.MainActivity
+import com.akshayashokcode.notepad.feature_note.presentation.util.Screen
+import com.akshayashokcode.notepad.ui.theme.NotePadTheme
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class NotesScreenTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @ExperimentalAnimationApi
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+        composeRule.setContent {
+            val navController = rememberNavController()
+            NotePadTheme {
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.NotesScreen.route
+                ) {
+                    composable(route = Screen.NotesScreen.route) {
+                        NotesScreen(navController = navController)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun clickToggleOrderSection_isVisible() {
+        for(i in 1..3) {
+            // Click on FAB to get to add note screen
+            composeRule.onNodeWithContentDescription("Add").performClick()
+
+            // Enter texts in title and content text fields
+            composeRule
+                .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
+                .performTextInput(i.toString())
+            composeRule
+                .onNodeWithTag(TestTags.CONTENT_TEXT_FIELD)
+                .performTextInput(i.toString())
+            // Save the new
+            composeRule.onNodeWithContentDescription("Save").performClick()
+        }
+        composeRule.onNodeWithTag(TestTags.ORDER_SECTION).assertDoesNotExist()
+        composeRule.onNodeWithContentDescription("Sort").performClick()
+        composeRule.onNodeWithTag(TestTags.ORDER_SECTION).assertIsDisplayed()
+    }
+}
